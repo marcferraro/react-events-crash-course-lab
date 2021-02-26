0873bbf85ec39d37f92f02699166ebeb85c55521
Marc <marc.ferraro1@gmail.com>
2021-02-26 13:19:03 -0600

    commit tree 9969445a1d514ce5d50a308e7d91f240ebcfcdf0

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -10,6 +10,17 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
+    // console.log(event.clientX, event.clientY)
+    drawChromeBoiAtCoords(event.clientX, event.clientY)
+  }
+
+  handleKeyDown = (event) => {
+    console.log(event.key)
+    if (event.key === 'a'){
+      resize('+')
+    } else if (event.key === "s"){
+      resize('-') 
+    }
   }
   
   /* TODO: Create an event handler which, when fired, invokes the provided
@@ -28,6 +39,8 @@ export default class ChromeBoisDomain extends Component {
     return (
       <canvas 
         onMouseMove={this.handleMouseMove}
+        onClick={toggleCycling}
+        onKeyDown={this.handleKeyDown}
         width='900'
         height='600'
         tabIndex="0">
